7eb1d60ee0a3ad7fc275ed1e6e52fc1cb17d5d7f
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2018-11-21 16:57:24 +0100

    Works

--- cep/src/main/scala/org.fiware.cosmos.orion.flink.cep/CEPMonitoring.scala
+++ cep/src/main/scala/org.fiware.cosmos.orion.flink.cep/CEPMonitoring.scala
@@ -4,16 +4,17 @@ import java.util.Properties
 
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.cep.nfa.AfterMatchSkipStrategy
 import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
-import org.fiware.cosmos.orion.flink.connector.{Entity, HttpBody, MapToAttributeConverter, OrionSource}
+import org.fiware.cosmos.orion.flink.connector._
 import org.json4s.DefaultFormats
 import org.json4s.jackson.JsonMethods.parse
 import org.slf4j.LoggerFactory
+import org.apache.flink.cep.scala.{CEP, PatternStream}
 import org.apache.flink.cep.scala.pattern.Pattern
-
 /**
   * CEP Monitoring
   *
@@ -24,30 +25,49 @@ object CEPMonitoring{
   val executionGraphProperties: Seq[String] =  Seq.empty
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-    // Create Orion Source. Receive notifications on port 9001
-    val properties = new Properties()
+
+    // Create Kafka Source
+   /* val properties = new Properties()
     properties.setProperty("bootstrap.servers", "138.4.7.94:9092")
     val usageControlConsumer = new FlinkKafkaConsumer011[String]("flink", new SimpleStringSchema(), properties)
-//    val kafkaStream = env.addSource(usageControlConsumer)
+    val kafkaStream = env.addSource(usageControlConsumer)
+   */
+
+    // Create Orion Source. Receive notifications on port 9001
     val orionStream = env.addSource(new OrionSource(9001, "flink"))
+
     val orionProcessedDataStream : DataStream[Entity] = orionStream
       .flatMap(event => event.entities)
 
-    val windowedStream =  orionProcessedDataStream.timeWindowAll(Time.seconds(10))
-      .aggregate(new CustomAggregator)
+
+//    val windowedStream =  orionProcessedDataStream
+//      .timeWindowAll(Time.seconds(10))
+//      .aggregate(new CustomAggregator)
 
 
 
-    val pattern = Pattern.begin[Entity]("start").where(_.id == "334")
-      .next("middle").subtype(classOf[Entity]).where(_.id == "10.0")
-      .followedBy("end").where(_.id == "end")
+    val comparableEntityDatastream : DataStream[ComparableEntity] = orionProcessedDataStream.map(entity => new ComparableEntity(entity))
+    val countPattern = Pattern.begin[Entity]("start",AfterMatchSkipStrategy.skipPastLastEvent()).timesOrMore(3).within(Time.seconds(10))
+    val zipCodePattern =countPattern  ./*where(_.attrs("presence").value == "true")*/ followedBy("middle")
+        .where((ent: Entity,ctx)=>  ctx.getEventsForPattern("start").map(e=>e.attrs("zip").value).toSet.size >= 2 )
 
-//    val patternStream = org.apache.flink.cep.CEP.pattern(orionProcessedDataStream, pattern)
-//
+    val pattern = zipCodePattern
+
+    val patternStream  = CEP.pattern( orionProcessedDataStream, pattern)
+
+    val orionDataStreamSink : DataStream[OrionSinkObject] = patternStream.select( pattern => {
+      (pattern.get("start").toList(0) ++ pattern.get("middle").toList(0)).toList})
+      .map(entity => {
+        new OrionSinkObject("{ \"data\": [{\"id\":\"home-03\",\"type\":\"home\",\"presence\":{\"type\":\"Boolean\",\"value\":\"true\",\"metadata\":{}},\"zip\":{\"type\":\"String\",\"value\":\"28026\",\"metadata\":{}},\"temperature\":{\"type\":\"float\",\"value\":53,\"metadata\":{}}}],  \"subscriptionId\": \"5bf57244e2752b23275b1cfb\" }",
+          "http://localhost:9002/",ContentType.JSON,HTTPMethod.POST)
+      })
+    
+    OrionSink.addSink(orionDataStreamSink)
+
 //    val result: DataStream[String] = patternStream.select("ALERT")
 
 
-    windowedStream.print()
+//    windowedStream.print()
 
 
 
