a11eb0bc480ff5a357a02829a7b2db895f0e1854
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2019-03-27 16:37:00 +0100

    Fix notification

--- cep/src/main/scala/org.fiware.cosmos.orion.flink.cep/CBRequests.scala
+++ cep/src/main/scala/org.fiware.cosmos.orion.flink.cep/CBRequests.scala
@@ -1,3 +1,5 @@
+package org.fiware.cosmos.orion.flink.cep
+
 import org.json4s.JsonAST.{JArray, JField, JObject, JString}
 import org.json4s.jackson.JsonMethods.parse
 import scalaj.http.Http
@@ -26,16 +28,13 @@ object CBRequests {
         JField("url",JString(url))<-http
         if notificationURL.equals(url)
         JField("id",JString(id))<-obj
-      }yield id(0)
+      }yield id
 
       return id(0)
     } catch {
       case _: Exception => null
       case _: Error => null
     }
-  }
-  def main(args: Array[String]): Unit = {
-    unsubscribe("http://138.4.22.138:1026/v2/subscriptions","http://138.4.7.110:9001/notify")
   }
 
 }
