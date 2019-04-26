90cf013e6ab569fef74ea5294d8ddca858339680
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2019-04-26 17:22:41 +0200

    Add docs

--- ptp/docker/cep/src/main/scala/org.fiware.cosmos.orion.flink.cep/connector/CEPParser.scala
+++ ptp/docker/cep/src/main/scala/org.fiware.cosmos.orion.flink.cep/connector/CEPParser.scala
@@ -17,6 +17,11 @@ object CEPParser {
   val endExecutionGraphPattern : Regex = ".*?org.apache.flink.runtime.jobmaster.JobMaster *-* *(.*)".r
   val jobIdPattern : Regex = ".*?org.apache.flink.runtime.jobmaster.JobManagerRunner *-* *JobManager runner for job .* \\\\((\\\\w*)\\\\) was .*".r
 
+  /**
+    * Converts HTTP message into Log
+    * @param req FullHttpRequest object
+    * @return parsed Log
+    */
   final def parseGenericMessage(req : FullHttpRequest) : Log = {
     val headerEntries = req.headers().entries()
     // Retrieve body content and convert from Byte array to String
@@ -27,7 +32,11 @@ object CEPParser {
     parse(jsonBodyString).extract[Log]
   }
 
-
+  /**
+    * Parse log content
+    * @param log Received Log
+    * @return either a NgsiEvent or an ExecutionGraph Object
+    */
   final def parseMessage(log : Log) : Either[NgsiEvent, ExecutionGraph] =  {
 
     try {
