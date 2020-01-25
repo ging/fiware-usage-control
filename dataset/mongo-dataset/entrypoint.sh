f0c94ace863bbdadd29bb160a65f9a0a2b85f801
anmunoz <andres.max36@gmail.com>
2020-01-25 15:31:21 +0100

    Update dataset container config

--- dataset/mongo-dataset/entrypoint.sh
+++ dataset/mongo-dataset/entrypoint.sh
@@ -3,7 +3,7 @@
 mongod --bind_ip 0.0.0.0 &
 sleep 4
 mongoimport --db superMarket --collection tickets --drop --file DelightingCustomersBD.json
-#rm DelightingCustomersBD.json
+rm DelightingCustomersBD.json
 touch /var/log/mongodb/nn.log
 
 sleep 10 && tail -f `ls /var/log/mongodb/*.log | head -n1`
